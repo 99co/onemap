19339662ba46357a82579ed4e5732860d71ddf90
Ruiwen Chua <ruiwen+github@thoughtmonkeys.com>
2014-11-28 22:58:41 +0800

    Update support files

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 from distutils.core import setup
 
+try:
+  import pypandoc
+  readme = pypandoc.convert("README.md", "rst")
+except (IOError, ImportError):
+  readme = open("README.md").read()
+
 setup(
     name='onemap',
-    version='0.1',
+    version='0.2.0',
     author='Ruiwen Chua',
     author_email='ruiwen@99.co',
     packages=['onemap'],
-    url='http://pypi.python.org/pypi/TowelStuff/',
+    url='https://github.com/99co/onemap',
     license='LICENSE.txt',
     description='Wrapper for the OneMap API',
-    long_description=open('README.txt').read(),
+    long_description=readme,
+    keywords=['onemap'],
     install_requires=[
         "requests >= 1.2.3"
-    ],
+    ]
 )
